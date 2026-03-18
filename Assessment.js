63e10a8ed082b84c76634a375149a73a0d3222bb
Anushka Pandey <anushkapandey7002@gmail.com>
2026-03-19 00:37:21 +0530

    Updated Files

---
+++ Assessment.js
@@ -0,0 +1,9 @@
+let student = [];
+function allstudents() {
+    let name = document.getElementById("name").value;
+    let roll = document.getElementById("roll").value;
+    let Marks_in_Maths = document.getElementById("Marks_in_Maths").value;
+    let Marks_in_Science = document.getElementById("Marks_in_Science").value;
+    let Marks_in_English = document.getElementById("Marks_in_English").value;
+    
+} 
