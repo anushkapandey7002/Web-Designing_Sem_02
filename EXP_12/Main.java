6c3f860021c5222b135f1085574b636085f442f8
Anushka Pandey <anushkapandey7002@gmail.com>
2026-05-29 08:39:44 +0530

    EXP_12

---
+++ EXP_12/Main.java
@@ -0,0 +1,21 @@
+Open postman
+Create Employee (POST)
+Body
+{
+"id":1,
+"name":"Abhishek Pratap Singh”
+"department":"CSE",
+"salary":50000
+}
+
+DELETE EMPLOYEE
+{
+"id":1,
+"name":"Abhishek Pratap Singh",
+"department":"CSE",
+"salary":50000
+}
+{"id":1,"name":"Abhishek Pratap Singh","department":"CSE","salary":50000}
+OUTCOME
+Students successfully implemented CRUD operations using Spring Boot REST APIs and connected the
+application with a database using Spring Data JPA
